d1aaafe14f316704e5e17985bd2b02f2fd9a2867
sushant <00sushant19@gmail.com>
2022-07-04 01:37:27 +0530

    made models

---
+++ oj/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Question,Submission, User
+
+admin.site.register(Question)
+admin.site.register(User)
+admin.site.register(Submission)
